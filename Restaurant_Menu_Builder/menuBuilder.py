2d2c2f34aef5e7840c6162169b94702cbc490b09
sandhyab1506 <131992478+sandhyab1506@users.noreply.github.com>
2023-05-26 11:46:07 +0100

    Update menuBuilder.py
    Included Header Information

--- Restaurant_Menu_Builder/menuBuilder.py
+++ Restaurant_Menu_Builder/menuBuilder.py
@@ -1,5 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#Licence: MIT
+#
+#Author(s): Sandhya Buddharaju
+#
+#Program purpose:
+#----------------
+# 
 # Task is to Display a Menu in the Format Specified below adhering the rules stated
-
+#
+#Program notes:
+#--------------
+#
+# -----------------------------------------------------------------------------------
+# Rules
+# -----------------------------------------------------------------------------------
+# 1) All the food categories and corresponding items are listed in food_categories
+# 2) Display only the items the chef can prepare . They are listed in chef_dishes
+# 3) Restaurant busy times can be determined by is_busy flag
+# 4) At busy times the Menu have to be displayed in ascending order based on prep times listed in prep_times
+#
+# We assume the intial data be provided and stored in a list
+#
+#--------------------------------------------------------
 """
 ==== Example Restaurant ====
       ~ Menu ~
@@ -13,13 +37,7 @@ Bento, Sushi, Pasta or Rice
 Desserts
 Apples, Strawberries or Cheese
 """
-# -----------------------------------------------------------------------------------
-# Rules
-# -----------------------------------------------------------------------------------
-# 1) All the food categories and corresponding items are listed in food_categories
-# 2) Display only the items the chef can prepare . They are listed in chef_dishes
-# 3) Restaurant busy times can be determined by is_busy flag
-# 4) At busy times the Menu have to be displayed in ascending order based on prep times listed in prep_times
+
 
 # importing required functions
 from Restaurant_Menu_Builder import menuFunctions
